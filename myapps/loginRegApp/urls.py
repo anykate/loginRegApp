c7ebddf35df6834c59578139858468b01194343c
Aniket Aryamane <aryamane.aniket@gmail.com>
2019-12-13 12:56:50 +0530

    Early steps to create user and redirect him to success page

--- myapps/loginRegApp/urls.py
+++ myapps/loginRegApp/urls.py
@@ -6,4 +6,6 @@ app_name = 'loginRegApp'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('createuser/', views.createuser, name='createuser'),
+    path('success/', views.success, name='success'),
 ]
--- myapps/loginRegApp/views.py
+++ myapps/loginRegApp/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def index(request):
     return render(request, 'loginRegApp/index.html', {})
+
+
+def createuser(request):
+    print(request.POST)
+    return redirect('loginRegApp:success')
+
+
+def success(request):
+    return render(request, 'loginRegApp/success.html', {})
